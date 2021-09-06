a6e60c7fb9a054b8a747819fad2e22c783f9d34d
tkaya <tkayasat@gmail.com>
2021-09-07 01:40:37 +0300

    Initial commit + добавлен ViewBinding

---
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment.newInstance()).commit()
+    }
+}
