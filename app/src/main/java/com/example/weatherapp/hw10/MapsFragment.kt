f2012d42c9890f40bb14109062804c6fb77ede00
tkaya <tkayasat@gmail.com>
2022-07-11 18:08:25 +0300

    hw10 ( по уроку, разобрано) - маркеры

--- app/src/main/java/com/example/weatherapp/hw10/MapsFragment.kt
+++ app/src/main/java/com/example/weatherapp/hw10/MapsFragment.kt
@@ -1,41 +1,87 @@
 package com.example.weatherapp.hw10
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 
 class MapsFragment : Fragment() {
 
     private lateinit var map: GoogleMap
 
-
     companion object {
         fun newInstance() = MapFragment()
     }
 
-    private var _binding: FragmentSearchMapBinding? = null
-    private val binding: FragmentSearchMapBinding
-        get() = _binding!!
+    private val markers: ArrayList<Marker> = arrayListOf()
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+
+        val startPlace = LatLng(55.0, 37.0)
+        map.addMarker(MarkerOptions().position(startPlace).title("Marker Start"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(startPlace))
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isZoomGesturesEnabled = true
+
+        val isPermissionGranted =
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) ==
+                    PackageManager.PERMISSION_GRANTED
+
+        map.setMyLocationEnabled(isPermissionGranted)
+        map.uiSettings.isMyLocationButtonEnabled = true
+
+        map.setOnMapLongClickListener { location ->
+            moveToPosition(location)
+            addMarker(location)
+            drawLine()
+        }
+    }
+
+    private fun addMarker(location: LatLng) {
+        markers.add(
+            map.addMarker(
+                MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map_pin))
+                    .position(location)
+                    .title("")
+            )!!
+        )
+    }
+
+    private fun drawLine() {
+        val last = markers.size - 1
+        if (last > 0) {
+            val startMarker = markers[last - 1].position
+            val endMarker = markers[last].position
+            map.addPolyline(
+                PolylineOptions()
+                    .add(startMarker, endMarker)
+                    .color(Color.RED)
+                    .width(5f)
+            )
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
-        _binding = FragmentSearchMapBinding.inflate(inflater, container, false)
+        _binding = FragmentGoogleMapsMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -43,66 +89,17 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-
         binding.buttonSearch.setOnClickListener {
             val geocoder = Geocoder(requireContext())
             val addressRow = binding.searchAddress.text.toString()
             val address = geocoder.getFromLocationName(addressRow, 1)
             val location = LatLng(address[0].latitude, address[0].longitude)
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
-
-            var addressString =
-                "${address[0].countryName}, ${address[0].locality}, ${address[0].thoroughfare}, ${address[0].subThoroughfare}"
-
-            map.addMarker(
-                MarkerOptions()
-                    .title(addressString)
-                    .position(location)
-            )
+            moveToPosition(location)
         }
     }
 
-    private val callback = OnMapReadyCallback {
-        map = it
+    private fun moveToPosition(location: LatLng) {
 
-        map.uiSettings.isZoomControlsEnabled = true
-        map.uiSettings.isZoomGesturesEnabled = true
-
-        checkPermission()
-
-        map.uiSettings.isMyLocationButtonEnabled = true
-
-        map.setOnMapLongClickListener { location ->
-            map.addMarker(
-                MarkerOptions()
-                    .title("")
-                    .position(location)
-            )
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private val requestFineLocationPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            if (isGranted) {
-                map.isMyLocationEnabled = true
-            }
-        }
-
-    fun checkPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                map.isMyLocationEnabled = true
-            }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                requestFineLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-            else -> {
-                requestFineLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
     }
 }
